dc152179bbfe2c58f92a8650dafabf1764f3d29d
Prajakta <109715943+Prajaktasangolkar@users.noreply.github.com>
2023-12-03 16:28:20 +0000

    22

---
+++ 03_basics/5.arrow.js
@@ -0,0 +1,39 @@
+const user={
+    username:'praju',
+    age:22,
+    welcomeMessage:function(){
+        console.log(`${this.username},welcome to website`);
+        console.log(this);
+    }
+}
+
+user.welcomeMessage()
+user.username='sam'
+user.welcomeMessage()
+// console.log(this);  //{} we will get this
+
+
+// We can't use this in function 
+function chai(){
+  let username='hii'
+  console.log(this.username); //this will give undefined value
+}
+chai()
+
+// so use arrow for this
+// but this is also give undefined as output
+// ++++++++++++++++Arrow+++++++++++++++++++++++++++++++++++++
+const chai1=()=>{
+    let username='Hitesh'
+    console.log(this.username);
+}
+chai1()
+
+// const addTwo=(num1,num2)=>{
+//   return num1+num2;
+// }
+// ----this is implicite function-----
+const addTwo=(num1,num2)=>num1+num2;
+
+
+console.log(addTwo(3,4));
