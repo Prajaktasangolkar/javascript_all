491663344366f7892a055e18bedde394a28318ed
Prajakta <109715943+Prajaktasangolkar@users.noreply.github.com>
2023-12-09 15:16:20 +0000

    Pending changes exported from your codespace

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,20 @@
+// this is like overriding 
+// like we are using super instead of this we are using 'this'
+// call and 'then' use
+
+function SetUsername(username){
+    //complex DB calls
+    this.username=username
+    console.log('called');
+}
+
+function createUser(username,email,password){
+    SetUsername.call(this,username)
+    this.email=email
+    this.password=password
+}
+
+const chai=new createUser("chai","chai@fb.com","123")
+console.log(chai);
+
+
--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -1,2 +1,65 @@
-let myName="Praju   "
-console.log(myName.length);
+// let myName="Praju   "
+// console.log(myName.length);
+// // console.log(myName.trim().length);
+// console.log(myName.truelength());
+
+let myHeros=['thor','spiderman']
+let heroPower={
+    thor:'hammer',
+    spiderman:'sling',
+    getSpiderPower:function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh=function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+
+Array.prototype.heyHitesh=function(){
+    console.log(`hitesh is hello`);
+}
+
+heroPower.hitesh()
+myHeros.hitesh()
+myHeros.heyHitesh()
+// heroPower.heyHitesh()  //not have power of array
+
+
+// ++++++++++++++inheritance+++++++++++++++
+
+const User={
+    name:'chai',
+    email:'chai@gmail.com'
+}
+const Teacher={
+    makeVideo:true
+}
+
+const TeachingSupport={
+    isAvailable:false
+}
+
+const TASupport={
+    makeAssignment:'JS assignment',
+    fulltime:true,
+    __proto__:TeachingSupport
+}
+
+Teacher.__proto__=User
+
+//modern structure
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+let anotherUsername="ChaiAurCode   "
+
+String.prototype.trueLength=function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+
+}
+
+
+anotherUsername.trueLength()
+'hiesh'.trueLength()
